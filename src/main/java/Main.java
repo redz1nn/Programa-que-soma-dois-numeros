754cf46f8f7ed71a30c7033936a95929121f6cb4
Victor Fabris <victor.fabris.santos@escola.pr.gov.br>
2024-05-27 12:34:30 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Programa que soma dois números");
+ 
+  System.out.println("o primeiro número é:");
+  int numero1 = 4;
+  
+  System.out.println(numero1);
+
+  System.out.println("o segundo número é:");
+  int numero2 = 6;
+
+  System.out.println(numero2);
+  System.out.println("o resultado é:");
+  int soma = numero1 + numero2;
+  System.out.println(soma);
+  
+  
+ } 
+}
